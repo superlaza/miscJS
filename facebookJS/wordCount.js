cb4536dafdbfbd9223d1b3e00152faf6f6e65362
David Perlaza <superlaza7@gmail.com>
2014-04-15 01:01:56 -0400

    Update wordCount.js

--- facebookJS/wordCount.js
+++ facebookJS/wordCount.js
@@ -1,18 +1,25 @@
+var jq = document.createElement('script');
+jq.src = "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
+document.getElementsByTagName('head')[0].appendChild(jq);
+// ... give time for script to load, then type.
+jQuery.noConflict();
+
 var theirTotal = 0;
 var yourTotal = 0;
 
 $(document).ready(function(){
-	var yourName = $("h1","#rhs").text();
-	var theirName = "Santiago Guisasola";
+	//var yourName = $("h1","#rhs").text();
+	var yourName = "Perlaza"
+	var theirName = "Bird";
 	$(".thread").each(function(){
-		if($(".profile",$(".header",$(this))).text().match(theirName)!== null){
-			$(".from",$(this)).each(function(){
+		if(this.innerHTML.slice(0,20).split(" ")[1].slice(0,-1) == theirName){
+			$(".message",$(this)).each(function(){
 				var words;
-				if($(".profile",$(this)).text()==theirName){
+				if($(".user",$(".message_header",$(this))).text().split(" ")[1]==theirName){
 					words = $(this).siblings(".msgbody").text();
 					theirTotal+=words.split(' ').length;
 				}
-				if($(".profile",$(this)).text()==yourName){
+				if($(".user",$(".message_header",$(this))).text().split(" ")[1]==yourName){
 					words = $(this).siblings(".msgbody").text();
 					yourTotal+=words.split(' ').length;
 				}
