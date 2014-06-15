93adcee77d1a0ef83a8c84c8ef2af7bd97ae7650
David Perlaza <superlaza7@gmail.com>
2014-06-15 16:07:53 -0400

    Update wordCount.js

--- facebookJS/wordCount.js
+++ facebookJS/wordCount.js
@@ -1,7 +1,7 @@
 /**
  * Created by David on 6/15/2014.
  */
-function jqLoad(){}
+function jqLoad(){
     var jq = document.createElement('script');
     jq.src = "http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js";
     document.getElementsByTagName('head')[0].appendChild(jq);
